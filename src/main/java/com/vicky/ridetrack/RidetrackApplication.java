fab3579428b84ebb16fe89f7d3655f6a66b7c287
Vicky Shetty <vickyshetty@Vickys-Mac-mini.local>
2026-03-29 20:06:31 +0530

    initial: Spring Boot project setup

---
+++ src/main/java/com/vicky/ridetrack/RidetrackApplication.java
@@ -0,0 +1,13 @@
+package com.vicky.ridetrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidetrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidetrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vicky/ridetrack/RidetrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicky.ridetrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidetrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
